2a822924d7eec12f1308fb1000a89cac22d8db19
Fred15 <frederik.borutta@gmail.com>
2025-07-20 16:45:14 +0200

    analyze to msci world value avolution

--- R/analyze_correlation.R
+++ R/analyze_correlation.R
@@ -7,6 +7,10 @@ library(base)
 
 msci_data_return <- read_csv("./data/MSCI_World_Return.csv")
 
+msci_data_evolution_unprepared <- read_csv("./data/MSCI_World_Evolution_in EUR.csv")
+msci_data_evolution <- msci_data_evolution_unprepared[grepl("^01\\.", msci_data_evolution_unprepared$Year), ]
+msci_data_evolution$Year <- sub("^01\\.", "", msci_data_evolution$Year)
+
 #' Analyze Correlation with MSCI World Returns
 #'
 #' This function checks whether the input data has a 'Year' column, merges it with MSCI World
@@ -28,13 +32,41 @@ msci_data_return <- read_csv("./data/MSCI_World_Return.csv")
 #'
 #' @export
 analyze_correlation_to_msci_world_returns <- function(input_data) {
+  return(analyze_correlation(msci_data_return, input_data))
+}
+
+#' Analyze Correlation with MSCI World Value Evolution
+#'
+#' This function checks whether the input data has a 'Year' column, merges it with MSCI World
+#' Index Value Evolution data, and calculates correlation and p-values for all numeric variables
+#' with respect to MSCI returns.
+#'
+#' @param input_data A data.frame containing at least a column named 'Year' and other numeric columns.
+#'
+#' @return A data.frame with variables, p-values, significance ('highly significant', 'very significant', 'significant', 'not significant'), r-values and correlation strength ('high', 'medium', 'low').
+#'
+#' @examples
+#' test_data <- data.frame(
+#'   Year = 2000:2020,
+#'   Inflation = runif(21, 0, 0.1),
+#'   GDP = runif(21, 1, 3)
+#' )
+#'
+#' result <- analyze_correlation_to_msci_world_value_evolution(test_data)
+#'
+#' @export
+analyze_correlation_to_msci_world_value_evolution <- function(input_data) {
+  return(analyze_correlation(msci_data_return, input_data))
+}
+
+analyze_correlation <- function(reference_data, input_data) {
   # Check if input data contains column named year
   if (!"Year" %in% names(input_data)) {
     stop("No Column named Year found")
   }
 
   # Join datasets on 'Year' column
-  merged_data <- merge(msci_data_return, input_data, by = "Year")
+  merged_data <- merge(reference_data, input_data, by = "Year")
   vars_to_check <- setdiff(names(merged_data), c("Year", "Return"))
 
   results <- data.frame(
